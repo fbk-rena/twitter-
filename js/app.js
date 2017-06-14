805d554b0db2a29b14eee30ec7f6f110fcbf85bf
fbk-rena <fbk.rena@gmail.com>
2017-06-14 00:36:53 -0500

    cambio de colores

--- js/app.js
+++ js/app.js
@@ -11,47 +11,64 @@ function cargarPagina() {
 };
 
 var printTweet = function (e) {
-     e.preventDefault();
-        //Conetnido del tweet
-        var $mensaje = $('#textarea1').val();
+    e.preventDefault();
+    //Conetnido del tweet
+    var $mensaje = $('#textarea1').val();
 
-        //Crear elementos
-        var $tarjeta = $("<div />", {
-            'class': 'row'
-        });
-        var $contenedorCol = $("<div/>", {
-            'class': 'col s 12 m6'
-        });
-        var $contenedor = $("<div/>", {
-            'class': 'card pink accent-2'
-        });
-        var $contenedorText = $("<div/>", {
-            'class': 'card-content white-text'
-        });
-        var $btnBorrar = $("<a/>", {
-            'class': 'btn-floating pink right'
-        });
-        var $icon = $("<i/>", {
-            'class': 'material-icons'
-        });
-        var $nuevoTweet = $("<p />").text($mensaje);
+    //Crear elementos
+    var $tarjeta = $("<div />", {
+        'class': 'row'
+    });
+    var $contenedorCol = $("<div/>", {
+        'class': 'col s 12 m6'
+    });
+    var $contenedor = $("<div/>", {
+        'class': 'card pink accent-2'
+    });
+    var $contenedorText = $("<div/>", {
+        'class': 'card-content white-text'
+    });
+    var $btnBorrar = $("<a/>", {
+        'class': 'btn-floating pink right'
+    });
+    var $icon = $("<i/>", {
+        'class': 'material-icons'
+    });
+    var $nuevoTweet = $("<p />").text($mensaje);
 
-        var $iconBorrar = $icon.text("delete");
+    var $iconBorrar = $icon.text("delete");
 
-        //armar tarjeta
-        $btnBorrar.append($iconBorrar);
-        $contenedorText.append($nuevoTweet, $btnBorrar);
-        $contenedor.append($contenedorText);
-        $contenedorCol.append($contenedor);
-        $tarjeta.append($contenedorCol);
+    //armar tarjeta
+    $btnBorrar.append($iconBorrar);
+    $contenedorText.append($nuevoTweet, $btnBorrar);
+    $contenedor.append($contenedorText);
+    $contenedorCol.append($contenedor);
+    $tarjeta.append($contenedorCol);
 
-        $("#tweets").prepend($tarjeta);
+    $("#tweets").prepend($tarjeta);
 
-        //funcionalidad borrar al boton
-        $btnBorrar.click(function () {
-                    $(this).parent().remove();
-        });
+    //funcionalidad borrar al boton
+    $btnBorrar.click(function () {
+        $(this).parent().remove();
+
+        contador = 0;
+    });
 };
-    
-$(document).ready(cargarPagina);
 
+var contarCaracteres = function () {
+    $("p").on("keypress", function () {
+            contador++
+            if (contador >= 0 && contador <= 20) {
+                $(this).addClass("green-text");
+            };
+            if (contador >= 21 && contador <= 30) {
+                $(this).removeClass("green-text");
+                $(this).addClass("teal-text");
+            };
+        } else if (contador >= 31 && contador <= 140) {
+            $(this).removeClass("teal-text");
+        };
+
+    });
+};
+$(document).ready(cargarPagina);
